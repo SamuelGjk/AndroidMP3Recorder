82916f7210197589aa9e9ca2011a0cba83b96e40
SamuelGjk <samuel.alva@outlook.com>
2017-11-24 13:53:38 +0800

    增加一种录音暂停的实现方式

--- sample/src/main/java/com/czt/mp3recorder/sample/MainActivity.java
+++ sample/src/main/java/com/czt/mp3recorder/sample/MainActivity.java
@@ -3,40 +3,93 @@ package com.czt.mp3recorder.sample;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.czt.mp3recorder.MP3Recorder;
 
 public class MainActivity extends Activity {
 
+	private static final int ONE_SECOND = 1000;
+
+	private TextView mTextStatus;
+
 	private MP3Recorder mRecorder = new MP3Recorder(new File(Environment.getExternalStorageDirectory(),"test.mp3"));
+
+	private long mRecordingDuration;
+	private SimpleDateFormat mDateFormat = new SimpleDateFormat("mm:ss", Locale.getDefault());
+	private Handler mHandler = new Handler();
+	private Runnable mRunnable = new Runnable() {
+		@Override
+		public void run() {
+			mRecordingDuration += ONE_SECOND;
+			mTextStatus.setText(mDateFormat.format(mRecordingDuration));
+			mHandler.postDelayed(this, ONE_SECOND);
+		}
+	};
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		Button startButton = (Button) findViewById(R.id.StartButton);
+
+		mTextStatus = findViewById(R.id.text_status);
+
+		Button startButton = findViewById(R.id.start_button);
 		startButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				try {
-					mRecorder.start();
+					if (!mRecorder.isRunning()) {
+						mRecorder.start();
+						mHandler.postDelayed(mRunnable, ONE_SECOND);
+					}
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
 			}
 		});
-		Button stopButton = (Button) findViewById(R.id.StopButton);
+
+		Button stopButton = findViewById(R.id.stop_button);
 		stopButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				mRecorder.stop();
+				if (mRecorder.isRunning()) {
+					mRecorder.stop();
+					mHandler.removeCallbacks(mRunnable);
+					mRecordingDuration = 0;
+				}
+			}
+		});
+
+		Button resumeButton = findViewById(R.id.resume_button);
+		resumeButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (mRecorder.isRunning() && !mRecorder.isRecording()) {
+					mRecorder.resume();
+					mHandler.postDelayed(mRunnable, ONE_SECOND);
+				}
+			}
+		});
+
+		Button pauseButton = findViewById(R.id.pause_button);
+		pauseButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (mRecorder.isRecording()) {
+					mRecorder.pause();
+					mHandler.removeCallbacks(mRunnable);
+				}
 			}
 		});
 	}
